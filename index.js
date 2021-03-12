485ea3040373f7bc03879c29dc029d56d16a6a9a
semperry <ryan.curtis88@gmail.com>
2021-03-12 16:20:32 -0700

    adds cors

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 
 const swapiRoutes = require("./routes/swapiRoutes");
 
 const port = process.env.PORT || 4000;
 const app = express();
 
+app.use(
+	cors({
+		allowedHeaders: "Access-Control-Allow-Origin",
+	})
+);
 app.use(swapiRoutes);
 
 app.listen(port, () => {
