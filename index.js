5cc8d17ae8979fc69d401d36a44ff9c3c3ac9753
semperry <ryan.curtis88@gmail.com>
2021-03-12 16:25:03 -0700

    adds cors

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 
-const corsConfig = require("./config/corsConfig");
 const swapiRoutes = require("./routes/swapiRoutes");
 
 const port = process.env.PORT || 4000;
 const app = express();
 
-app.use([corsConfig, swapiRoutes]);
+app.use(
+	cors({
+		allowedHeaders: "Access-Control-Allow-Origin",
+	})
+);
+app.use(swapiRoutes);
 
 app.listen(port, () => {
 	console.log(`Redis cache server running on port ${port}`);
